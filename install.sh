3a57c65b86437b61de851863f2ce650ebfa6b9eb
ickerio <harry.ickeringill@gmail.com>
2023-06-02 02:52:21 +1000

    More readable and laptop optional

--- install.sh
+++ install.sh
@@ -2,27 +2,59 @@
 
 echo "Preparing to install ickerio/dotfiles"
 
-echo "Installing packages"
-sudo pacman -S hyprland hyprpaper kitty nautilus code openssh
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
 
-echo "Installing yay"
-pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-rm -r yay
+install_packages() {
+    echo "Installing packages"
+    sudo pacman -S hyprland hyprpaper kitty nautilus code openssh nano
+}
 
-echo "Installing AUR packages"
-yay -S waybar-hyprland-git brave-bin otf-font-awesome
+install_laptop_gpu_power_saving() {
+    echo "Laptop GPU power saving"
+    sudo pacman -S bbswitch bumblebee
+    sudo systemctl enable bumblebeed.service
+}
 
-echo "Creating symlinks"
-ln -s "$(pwd)/hypr" ~/.config/hypr
-ln -s "$(pwd)/kitty" ~/.config/kitty
-ln -s "$(pwd)/waybar" ~/.config/waybar
-ln -s "$(pwd)/wallpaper.png" ~/wallpaper.png
+install_yay() {
+    echo "Installing yay"
+    pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    rm -r yay
+}
 
-ln -s "$(pwd)/Nordic/gtk-3.0" ~/.config/gtk-3.0
-ln -s "$(pwd)/Nordic/gtk-4.0" ~/.config/gtk-4.0
+install_aur_packages() {
+    echo "Installing AUR packages"
+    yay -S waybar-hyprland-git brave-bin otf-font-awesome
+}
 
-echo "Generating key"
-ssh-keygen
+create_symlinks() {
+    echo "Creating symlinks"
+    ln -s "$(pwd)/hypr" ~/.config/hypr
+    ln -s "$(pwd)/kitty" ~/.config/kitty
+    ln -s "$(pwd)/waybar" ~/.config/waybar
+    ln -s "$(pwd)/wallpaper.png" ~/wallpaper.png
+
+    ln -s "$(pwd)/Nordic/gtk-3.0" ~/.config/gtk-3.0
+    ln -s "$(pwd)/Nordic/gtk-4.0" ~/.config/gtk-4.0
+}
+
+generate_key() {
+    echo "Generating key"
+    ssh-keygen
+}
+
+# Check if "laptop" flag was passed
+if [[ "$1" == "laptop" ]]; then
+    install_laptop_gpu_power_saving
+fi
+
+install_packages
+install_yay
+install_aur_packages
+create_symlinks
+generate_key
