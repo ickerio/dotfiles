88c14fcdd8280b9c219ad7c34e7c889b0a913c6e
ickerio <harry.ickeringill@gmail.com>
2023-06-02 01:18:28 +1000

    Keygen and symlink issue

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ I don't reccomend you use as WIP
 
 ## Setup
  - Execute `install.sh`
- - Add git config `git config --global user.name "John Doe"` and `git config --global user.email "johndoe@example.com"`
+ - Add git config `git config --global user.name "ickerio"` and `git config --global user.email "private@email.com"`
+ - Paste `/home/ickerio/.ssh/id_rsa.pub` on GitHub
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Preparing to install ickerio/dotfiles"
 
 echo "Installing packages"
-pacman -S hyprland hyprpaper waybar kitty dolphin code
+pacman -S hyprland hyprpaper waybar kitty dolphin code openssh
 
 echo "Installing yay"
 pacman -S --needed git base-devel
@@ -16,7 +16,10 @@ echo "Installing AUR packages"
 yay -S brave-bin otf-font-awesome
 
 echo "Creating symlinks"
-ln -s "$(pwd}hypr" ~/.config/hypr
-ln -s "$(pwd}kitty" ~/.config/kitty
-ln -s "$(pwd}waybar" ~/.config/waybar
-ln -s "$(pwd}wallpaper.png" ~/wallpaper.png
+ln -s "$(pwd)hypr" ~/.config/hypr
+ln -s "$(pwd)kitty" ~/.config/kitty
+ln -s "$(pwd)waybar" ~/.config/waybar
+ln -s "$(pwd)wallpaper.png" ~/wallpaper.png
+
+echo "Generating key"
+ssh-keygen
