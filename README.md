d47930740e384547c5076e1e7315c5c739fdeed2
ickerio <harry.ickeringill@gmail.com>
2023-06-02 02:04:24 +1000

    nautilus plus kitty copypaste

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # ickerio/dotfiles
 
-Hyprland with Waybar, Hyprpaper, Brave, Dolphin & Kitty
-
-I don't reccomend you use as WIP
-
 ## Setup
  - Execute `install.sh`
  - Add git config `git config --global user.name "ickerio"` and `git config --global user.email "private@email.com"`
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Preparing to install ickerio/dotfiles"
 
 echo "Installing packages"
-pacman -S hyprland hyprpaper waybar kitty dolphin code openssh
+sudo pacman -S hyprland hyprpaper kitty nautilus code openssh
 
 echo "Installing yay"
 pacman -S --needed git base-devel
@@ -13,13 +13,16 @@ makepkg -si
 rm -r yay
 
 echo "Installing AUR packages"
-yay -S brave-bin otf-font-awesome
+yay -S waybar-hyprland-git brave-bin otf-font-awesome
 
 echo "Creating symlinks"
-ln -s "$(pwd)hypr" ~/.config/hypr
-ln -s "$(pwd)kitty" ~/.config/kitty
-ln -s "$(pwd)waybar" ~/.config/waybar
-ln -s "$(pwd)wallpaper.png" ~/wallpaper.png
+ln -s "$(pwd)/hypr" ~/.config/hypr
+ln -s "$(pwd)/kitty" ~/.config/kitty
+ln -s "$(pwd)/waybar" ~/.config/waybar
+ln -s "$(pwd)/wallpaper.png" ~/wallpaper.png
+
+ln -s "$(pwd)/Nordic/gtk-3.0" ~/.config/gtk-3.0
+ln -s "$(pwd)/Nordic/gtk-4.0" ~/.config/gtk-4.0
 
 echo "Generating key"
 ssh-keygen
