e9fd2eb5b547e670d7640762b92dc1780b2e71aa
ickerio <harry.ickeringill@gmail.com>
2023-06-02 01:06:19 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ickerio/dotfiles
+
+Hyprland with Waybar, Hyprpaper, Brave, Dolphin & Kitty
+
+I don't reccomend you use as WIP
---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Preparing to install ickerio/dotfiles"
+
+echo "Installing packages"
+pacman -S hyprland hyprpaper waybar kitty dolphin code
+
+echo "Installing yay"
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+rm -r yay
+
+echo "Installing AUR packages"
+yay -S brave-bin otf-font-awesome
+
+echo "Creating symlinks"
+ln -s "$(pwd}hypr" ~/.config/hypr
+ln -s "$(pwd}kitty" ~/.config/kitty
+ln -s "$(pwd}waybar" ~/.config/waybar
+ln -s "$(pwd}wallpaper.png" ~/wallpaper.png
