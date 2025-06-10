83d47c3c83b6d5053f51795b2e683db244c719ab
ickerio <harry.ickeringill@gmail.com>
2025-06-10 20:27:06 +0800

    remove garbage

--- install.sh
+++ install.sh
@@ -9,19 +9,13 @@ fi
 
 install_packages() {
     echo "Installing packages"
-    sudo pacman -S hyprland hyprpaper kitty nautilus code openssh nano
+    sudo pacman -S nvidia hyprland hyprpaper kitty nautilus openssh nano
 }
 
 enable_bluetooth() {
     echo "Enabling bluetooth"
     sudo pacman -S bluez bluez-utils
     sudo systemctl enable bluetooth.service
-}
-
-install_laptop_gpu_power_saving() {
-    echo "Laptop GPU power saving"
-    sudo pacman -S bbswitch bumblebee
-    sudo systemctl enable bumblebeed.service
 }
 
 install_yay() {
@@ -35,29 +29,20 @@ install_yay() {
 
 install_aur_packages() {
     echo "Installing AUR packages"
-    yay -S waybar-hyprland-git brave-bin otf-font-awesome
+    yay -S cursor-bin brave-bin aylurs-gtk-shell
 }
 
 create_symlinks() {
     echo "Creating symlinks"
     ln -s "$(pwd)/hypr" ~/.config/hypr
     ln -s "$(pwd)/kitty" ~/.config/kitty
-    ln -s "$(pwd)/waybar" ~/.config/waybar
     ln -s "$(pwd)/wallpaper.png" ~/wallpaper.png
-
-    ln -s "$(pwd)/Nordic/gtk-3.0" ~/.config/gtk-3.0
-    ln -s "$(pwd)/Nordic/gtk-4.0" ~/.config/gtk-4.0
 }
 
 generate_key() {
     echo "Generating key"
     ssh-keygen
 }
-
-# Check if "laptop" flag was passed
-if [[ "$1" == "laptop" ]]; then
-    install_laptop_gpu_power_saving
-fi
 
 install_packages
 enable_bluetooth
